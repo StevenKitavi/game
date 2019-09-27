6bf987a852e69eb0f351db3f6a481cd41dab93ed
StevenKitavi <steven.kitavi@gmail.com>
2019-09-27 21:31:48 +0300

    React game X and O's game

---
+++ build/precache-manifest.80629aa8709b24305ca97acf1019a608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fff613fc1a5014b9e379f9d7edceecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf8b078994626307062",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6136646b6ee960a88f0a",
+    "url": "/static/js/2.7b60cdfd.chunk.js"
+  },
+  {
+    "revision": "2bf8b078994626307062",
+    "url": "/static/js/main.79736073.chunk.js"
+  },
+  {
+    "revision": "b2ae0f5f0da5a6de3cdf",
+    "url": "/static/js/runtime-main.e56c4438.js"
+  }
+]);
